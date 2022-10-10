7d3d4f57baf326f9b3ab84866f688f644ecb3d08
jeferson gomes <jeferson.gomes@sciensa.com>
2022-10-10 13:30:35 -0300

    Update Design Patter Java Strategy Concluido

--- src/main/java/PadroesComportamentais/Strategy/MainStrategy.java
+++ src/main/java/PadroesComportamentais/Strategy/MainStrategy.java
@@ -6,36 +6,37 @@ import PadroesComportamentais.Strategy.Interfaces.Imp_Interfaces.FreteExpresso;
 public class MainStrategy {
     public static void main(String[] args) {
 
+        //Criação do tipos de frete - Instanciação.
         FreteComum freteComum = new FreteComum();
         FreteExpresso freteExpresso = new FreteExpresso();
 
-        PedidoFreteMoveis pedidoFreteMoveis = new PedidoFreteMoveis();
-        pedidoFreteMoveis.setValor(1100);
-        pedidoFreteMoveis.setFrete(freteExpresso);
-        pedidoFreteMoveis.setFrete(freteComum);
-
+        //Criação de um pedido do tipo setor Eletronicos.
         PedidoFreteEletronicos pedidoFreteEletronicos = new PedidoFreteEletronicos();
+        //Atribuição do valor
         pedidoFreteEletronicos.setValor(2100);
-        pedidoFreteEletronicos.setFrete(freteExpresso);
+        //Definição do frete comum como sendo o frete escolhido.
         pedidoFreteEletronicos.setFrete(freteComum);
 
+        //Calculo do frete comum.
         System.out.println("DESIGN PATTERNS JAVA\n" +
-                "*** STRATEGY ***\n" +
-                "--- --- --- --- ---\n");
-
-        System.out.println("Setor: " + pedidoFreteMoveis.getSETOR());
-        System.out.println("Frete Comun: R$: " + pedidoFreteMoveis.calculaFrete());
-        try {
-            System.out.println("Frete Expresso: R$: " + pedidoFreteMoveis.calculaFrete());
-        }
-        catch (Exception e){
-            System.out.println(e);
-        }
-        System.out.println("--- --- ---");
-
+                "*** STRATEGY ***");
         System.out.println("Setor: " + pedidoFreteEletronicos.getSETOR());
         System.out.println("Frete Comun: R$: " + pedidoFreteEletronicos.calculaFrete());
+        //Calculo do frete expresso.
+        pedidoFreteEletronicos.setFrete(freteExpresso);
         System.out.println("Frete Expresso: R$: " + pedidoFreteEletronicos.calculaFrete());
+        System.out.println("--- --- ---");
+        //--- --- --- --- ---
+
+        PedidoFreteMoveis pedidoFreteMoveis = new PedidoFreteMoveis();
+        pedidoFreteMoveis.setValor(1100);
+        pedidoFreteMoveis.setFrete(freteComum);
+        System.out.println("DESIGN PATTERNS JAVA\n" +
+                "*** STRATEGY ***");
+        System.out.println("Setor: " + pedidoFreteMoveis.getSETOR());
+        System.out.println("Frete Comun: R$: " + pedidoFreteMoveis.calculaFrete());
+        pedidoFreteMoveis.setFrete(freteExpresso);
+        System.out.println("Frete Expresso: R$: " + pedidoFreteMoveis.calculaFrete());
         System.out.println("--- --- ---");
 
     }
